ffdc90aa62431d97391f0d9394c609541a4a5938
Li Yuanqing <39945542+li-yq@users.noreply.github.com>
2023-07-12 08:35:25 +0000

    setup irkernel

---
+++ containers/.devcontainer/irkernel/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -i
+
+USERNAME=${USERNAME:-${_REMOTE_USER:-"automatic"}}
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u "${CURRENT_USER}" >/dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u "${USERNAME}" >/dev/null 2>&1; then
+    USERNAME=root
+fi
+
+echo "Register IRkernel..."
+su "${USERNAME}" -c 'export PATH="/home/'"${USERNAME}"'/.local/bin:/root/.local/bin:${PATH}"; R -q -e "IRkernel::installspec()"'
